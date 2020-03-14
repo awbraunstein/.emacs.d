0447f68efbcee7b6bf754c86d2423f42aeb99eb1
Andrew Braunstein <awbraunstein@gmail.com>
2020-03-13 21:30:37 -0700

    Add the two files I forgot.

---
+++ custom.el
@@ -0,0 +1,16 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (rust-mode cython-mode tide dockerfile-mode company-go go-mode company guide-key web-mode smex flycheck magit exec-path-from-shell use-package)))
+ '(solarized-use-variable-pitch nil)
+ '(x-underline-at-descent-line t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
